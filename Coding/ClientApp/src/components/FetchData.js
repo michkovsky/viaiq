636686ab5f216074968a9174114450f053340862
Serhiy Michkovskyi <michkovsky@gmail.com>
2018-07-13 18:33:17 +0300

    modify ui project template to use service (#4)

--- Coding/ClientApp/src/components/FetchData.js
+++ Coding/ClientApp/src/components/FetchData.js
@@ -1,37 +1,53 @@
 import React, { Component } from 'react';
+import { Label, InputGroup } from 'react-bootstrap';
 
 export class FetchData extends Component {
   displayName = FetchData.name
 
   constructor(props) {
     super(props);
-    this.state = { forecasts: [], loading: true };
+    this.state = { tweets: [], loading: false, startDate: "2017-01-01", endDate: "2018-01-01", };
+    this.fetchTwitterData = this.fetchTwitterData.bind(this);
+    this.dateChange=this.dateChange.bind(this);
 
-    fetch('api/SampleData/WeatherForecasts')
+  }
+  fetchTwitterData() {
+    this.state.loading = true;
+    this.setState(this.state);
+    var url=["api/TwitterData/GetTweets?startDate=",this.state.startDate,"&endDate=",this.state.endDate].join();
+    fetch(url)
       .then(response => response.json())
       .then(data => {
-        this.setState({ forecasts: data, loading: false });
+        this.state.tweets=data;
+        this.state.loading=false;
+        this.setState(this.state);
       });
   }
+  dateChange(evt){
+    //it's too late to introduce redux
+    var element = evt.target;
+    var name=element.name;
+    var val = element.value;
+    this.state[name]=val;
+    this.setState(this.state);
+  }
 
-  static renderForecastsTable(forecasts) {
+  static renderTweetsTable(tweets) {
     return (
       <table className='table'>
         <thead>
           <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
+            <th>id</th>
+            <th>stamp</th>
+            <th>text</th>
           </tr>
         </thead>
         <tbody>
-          {forecasts.map(forecast =>
-            <tr key={forecast.dateFormatted}>
-              <td>{forecast.dateFormatted}</td>
-              <td>{forecast.temperatureC}</td>
-              <td>{forecast.temperatureF}</td>
-              <td>{forecast.summary}</td>
+          {tweets.map(tweets =>
+            <tr key={tweets.id}>
+              <td>{tweets.id}</td>
+              <td>{tweets.stamp}</td>
+              <td>{tweets.text}</td>
             </tr>
           )}
         </tbody>
@@ -42,12 +58,18 @@ export class FetchData extends Component {
   render() {
     let contents = this.state.loading
       ? <p><em>Loading...</em></p>
-      : FetchData.renderForecastsTable(this.state.forecasts);
+      : FetchData.renderTweetsTable(this.state.tweets);
 
     return (
       <div>
-        <h1>Weather forecast</h1>
+        <h1>Render tweets</h1>
         <p>This component demonstrates fetching data from the server.</p>
+        <InputGroup><Label>Input date range</Label>
+          <br />
+          <Label>Start Date</Label><input type="date" name="startDate"  value={this.state.startDate} onChange={this.dateChange} /><br />
+          <Label>End Date</Label><input type="date" name="endDate"  value={this.state.endDate} onChange={this.dateChange}  /><br />
+          <button onClick={this.fetchTwitterData}>fetch data</button>
+        </InputGroup>
         {contents}
       </div>
     );
